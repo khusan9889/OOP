5e637ac48ed078afc9f9497963626af5ffcdd70c
khusan9889 <k.xusan_2003@mail.ru>
2022-04-28 02:50:04 +0500

    ProjectWork on University Management System

---
+++ ProjectWork/ProjectWork/ProjectWork.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <cstdlib>
+using namespace std;
+
+class university {
+private:
+public:
+	void menu();
+	void schools();
+	void admission();
+	/*
+	void building();
+	void professors();
+	void staff();
+	*/
+};
+
+void university::menu()
+{
+	int choice;
+	char x;
+	system("cls");
+
+	cout << "\t\t\t ________________________________________ " << endl;
+	cout << "\t\t\t|      UNIVERSITY MANAGEMENT SYSTEM      |" << endl;
+	cout << "\t\t\t|________________________________________|" << endl;
+	cout << "\t\t\t 1.Schools of university" << endl;
+	cout << "\t\t\t 2.Admission criteria" << endl;
+	cout << "\t\t\t 3.About building" << endl;
+	cout << "\t\t\t 4.Our professors" << endl;
+	cout << "\t\t\t 5.Staff" << endl;
+
+	cout << "\t\t\t -------------------------------- " << endl;
+	cout << "\t\t\t |  Choose option:[1/2/3/4/5]   | " << endl;
+	cout << "\t\t\t -------------------------------- " << endl;
+	cout << "Enter Your Choose: ";
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		schools();
+		break;
+	case 2:
+		admission();
+		break;
+	/*
+	case 3:
+		building();
+		break;
+
+	case 4:
+		professors();
+		break;
+
+	case 5:
+		staff();
+		break;
+	*/
+	default:
+		cout << "\n\t\t\t INVALID choice... PLease try again :)";
+		break;
+	}
+}
+
+void university::schools()
+{
+	cout << "\t _____________________________________________________________________" << endl;
+	cout << "\t\t\t There are 3 faculties un INHA university.\n\t\tThey are:\n\t\t1.SOCIE-School of computer and information engineering.\n\t\t2.SOL-School of Logistics.\n\t\t3.SBL- School of business logistics.  " << endl;
+	cout << "\t ---------------------------------------------------------------------" << endl;
+}
+
+void university::admission()
+{
+	cout << "\t ______________________________________________________________________________________________" << endl;
+	cout << "\t\t\t IUT have 3admissions.\n\t\t1.In spring.\n\t\t2.At the beginning of summer(this is only for Grant based admission).\n\t\t3.At the middle of summer.\n\t\tExamination subjects can be changed each year, so we advice to check it in web site https://inha.uz/ \n\t\tP.S You have to have IELTS certificate at least 5.0 for SOCIE and 5.5 for SBL and SOL faculties.   " << endl;
+	cout << "\t ----------------------------------------------------------------------------------------------" << endl;
+}
+
+/*void building()
+{
+
+}
+
+void professors()
+{
+
+}
+
+void staff()
+{
+
+}
+*/
+int main()
+{
+	university system;
+	system.menu();
+
+	
+	return 0;
+}
---
+++ lab-9(2sem)/lab-9(2sem)/U2110155.cpp
@@ -0,0 +1,128 @@
+﻿//id: U2110155
+//name: Kadirov Khusan
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+class Person {
+    string name;
+    int age;
+public:
+    void getname(string n, int a) {
+        cout << "Please enter name and age of person: " << endl;
+        cin >> n >> a;
+        name = n;
+        age = a;
+    }
+    string printname() {
+        return name;
+    }
+    int printage() {
+        return age;
+    }
+};
+void input() {
+    Person h;
+    fstream file;
+    h.getname("hh", 17);
+    file.open("cpp.dat", ios::app | ios::binary);
+    file.write((char*)(&h), sizeof(h));
+    file.close();
+}
+
+int main()
+{
+    fstream iffile;
+    fstream ffile;
+    ffile.open("temp.dat", ios::app | ios::out | ios::in | ios::binary);
+    Person h;
+    string name1;
+    int j, age1, pos;
+    //input();
+    fstream infile;
+    infile.open("cpp.dat", ios::in | ios::app | ios::binary | ios::out);
+    cout << "Please enter: " << endl;
+    cout << "1.To Determine the age of the specified person." << endl;
+    cout << "2.To Determine the name if age is known." << endl;
+    cout << "3.To Delete a record." << endl;
+    cout << "4.To add a recor to a specific position." << endl;
+    cout << "5.To exit program." << endl;
+    cin >> j;
+    switch (j)
+    {
+    case 1:
+
+        cout << "Please enter the name of person to see age: ";
+        cin >> name1;
+        infile.seekg(0);
+        while (infile.read((char*)(&h), sizeof(h)))
+        {
+            if (h.printname() == name1)
+            {
+                cout << name1 << "'s age: " << h.printage() << endl;
+            }
+        }
+        return main();
+    case 2:
+        cout << "Please enter the age of person to see name: ";
+        cin >> age1;
+        infile.seekg(0);
+        while (infile.read((char*)(&h), sizeof(h))) {
+            if (h.printage() == age1) {
+                cout << h.printname() << " is " << age1 << " years old" << endl;
+            }
+        }
+        return main();
+    case 3:
+
+        int n;
+        cout << "Please enter 1 if you want to delete record by age and 2 if you want ro delete recor by name: ";
+        cin >> n;
+        if (n == 1) {
+            cout << "Please enter the age of person: ";
+            cin >> age1;
+            infile.seekg(0);
+            while (infile.read((char*)(&h), sizeof(h))) {
+                if (h.printage() != age1) {
+
+                    ffile.write((char*)&h, sizeof(h));
+                }
+            }
+            ffile.close();
+            remove("cpp.dat");
+            rename("temp.dat", "cpp.dat");
+        }
+        if (n == 2) {
+            cout << "Please enter the name of person: ";
+            cin >> name1;
+            infile.seekg(0);
+            while (infile.read((char*)(&h), sizeof(h))) {
+                if (h.printname() != name1) {
+
+                    ffile.write((char*)&h, sizeof(h));
+                }
+            }
+            ffile.close();
+            remove("cpp.dat");
+            rename("temp.dat", "cpp.dat");
+        }
+        return main();
+    case 4:
+        cout << "Please enter desired position to add a record: ";
+        cin >> pos;
+
+        h.getname("hh", 17);
+        iffile.seekp((pos - 1) / sizeof(h));
+        iffile.open("cpp.dat", ios::app | ios::binary);
+        iffile.write((char*)(&h), sizeof(h));
+        iffile.close();
+        return main();
+    case 5:
+        break;
+    default:
+        cout << "You have entered wrong number please try again(" << endl;
+        return main();
+    }
+    infile.close();
+    return 0;
+}
